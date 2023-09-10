2e22e6b2bb75803e9bb088fa71e6c59745ac9e58
Toni Isotalo <toni.isotalo@hotmail.com>
2023-09-10 08:30:37 +0300

    robots.txt and sitemap

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("robot.txt", views.robot),
+    path("sitemap", views.sitemap)
 ]
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def robot(request):
+    f = open("resources/robot.txt")
+    text = f.read()
+    return HttpResponse(text)
+
+def sitemap(request):
+    f = open("resources/sitemap.xml")
+    text = f.read()
+    return HttpResponse(text, content_type='text/xml')
